ce85859017c2c21024fb07cfee51362061b049c9
mazenhadjali <hadjalimazen@gmail.com>
2025-05-12 12:50:04 +0100

    edit pages disabled

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,10 +43,7 @@ const config = {
       ({
         docs: {
           sidebarPath: './sidebars.js',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/mazenhadjali/form-fate-doc/tree/main/',
+          // editUrl:'https://github.com/mazenhadjali/form-fate-doc/tree/main/',
         },
         // blog: {
         //   showReadingTime: true,
